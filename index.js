bd065a334a958c305f9c3e3dbfcecea49bb67d4d
SigmaApfel01 <henricky2107@gmail.com>
2026-05-24 10:48:21 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+const fs = require("fs");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
+});
+
+// Daten speichern
+let data = {};
+const DATA_FILE = "./data.json";
+
+// Laden
+if (fs.existsSync(DATA_FILE)) {
+  data = JSON.parse(fs.readFileSync(DATA_FILE));
+}
+
+function saveData() {
+  fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2));
+}
+
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+
+  const userId = message.author.id;
+  const today = new Date().toDateString();
+
+  if (!data[userId]) {
+    data[userId] = {
+      streak: 1,
+      lastDay: today,
+    };
+  } else {
+    if (data[userId].lastDay !== today) {
+      data[userId].streak += 1;
+      data[userId].lastDay = today;
+    }
+  }
+
+  saveData();
+
+  const streak = data[userId].streak;
+
+  try {
+    await message.member.setNickname(`🔥${streak} ${message.author.username}`);
+  } catch (err) {
+    console.log("Nickname Fehler:", err);
+  }
+});
+
+client.login("DEIN_BOT_TOKEN_HIER");
